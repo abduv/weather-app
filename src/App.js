fdf677ccd6214b1621dcbcbcf8c29e66ed1e02fc
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2021-12-26 13:52:28 +0600

    added react-query for API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react'
+import {useQuery} from 'react-query'
 import {DailyForecast} from './components/DailyForecast'
 import {Input} from './components/Input'
+import {Loader} from './components/Loader'
 import {MainContent} from './components/MainContent'
 import {API_KEY} from './constants'
 
@@ -10,52 +12,68 @@ function App() {
     const [currentDailyForecast, setCurrentDailyForecast] = useState([])
     const [currentCity, setCurrentCity] = useState('Almaty')
 
+    const fetchWeather = async ({queryKey}) => {
+        const [key, {API_KEY: api_key, currentCity: city, units: u, days}] = queryKey
+        let res
+
+        if (key === 'currentWeather') {
+            res = await fetch(`https://api.weatherbit.io/v2.0/current?key=${api_key}&city=${city}&units=${u}`)
+        } else if (key === 'currentDailyForecast') {
+            res = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?key=${API_KEY}&city=${currentCity}&units=${units}&days=${days}`)
+        }
+
+        if (!res.ok) {
+            throw new Error('Error')
+        }
+        return res.json()
+    }
+
+    const currentWeatherData = useQuery(['currentWeather', {API_KEY, currentCity, units}], fetchWeather)
+
     useEffect(() => {
-        fetch(
-            `https://api.weatherbit.io/v2.0/current?key=${API_KEY}&city=${currentCity}&units=${units}`
-        )
-            .then(json => json.json())
-            .then(data => {
+        if (currentWeatherData.data) {
+            const {
+                rh, wind_spd, wind_dir, temp, app_temp, dewpt, weather, vis, pres,
+            } = currentWeatherData.data.data[0]
+            setCurrentWeather({
+                humidity: rh,
+                wind_spd, wind_dir, temp,
+                feelsLikeTemp: app_temp,
+                dewPoint: dewpt,
+                visibility: vis,
+                barometer: pres * 0.0295301,
+                weather,
+            })
+        }
+    }, [currentWeatherData.data])
+
+    const currentDailyForecastData = useQuery(['currentDailyForecast', {API_KEY, currentCity, units, days: 4}], fetchWeather)
+
+    useEffect(() => {
+        if (currentDailyForecastData.data) {
+            const dailyForecast = currentDailyForecastData.data.data.map(day => {
                 const {
-                    rh, wind_spd, wind_dir, temp, app_temp, dewpt, weather, vis, pres,
-                } = data.data[0]
+                    valid_date,
+                    weather: {icon, description},
+                    high_temp,
+                    low_temp,
+                } = day
 
-                const currentWeather = {
-                    humidity: rh,
-                    wind_spd, wind_dir, temp,
-                    feelsLikeTemp: app_temp,
-                    dewPoint: dewpt,
-                    visibility: vis,
-                    barometer: pres * 0.0295301,
-                    weather,
+                return {
+                    date: valid_date,
+                    icon, description,
+                    dayTimeTemp: high_temp,
+                    nightTimeTemp: low_temp,
                 }
-                setCurrentWeather(currentWeather)
             })
 
-        fetch(
-            `https://api.weatherbit.io/v2.0/forecast/daily?key=${API_KEY}&city=${currentCity}&units=${units}&days=4`
-        )
-            .then(json => json.json())
-            .then(data => {
-                const dailyForecast = data.data.map(day => {
-                    const {
-                        valid_date,
-                        weather: {icon, description},
-                        high_temp,
-                        low_temp,
-                    } = day
+            setCurrentDailyForecast(dailyForecast)
+        }
+    }, [currentDailyForecastData.data])
 
-                    return {
-                        date: valid_date,
-                        icon, description,
-                        dayTimeTemp: high_temp,
-                        nightTimeTemp: low_temp,
-                    }
-                })
-
-                setCurrentDailyForecast(dailyForecast)
-            })
-    }, [currentCity, units])
+    if (currentWeatherData.isLoading || currentDailyForecastData.isLoading) {
+        return <Loader />
+    }
 
     return (
         <div className="w-screen h-screen bg-nightBg bg-center bg-cover bg-no-repeat">
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+    return (
+        <div className="flex items-center justify-center pt-8">
+            <div className="w-40 h-40 border-t-4 border-b-4 border-blue-700 rounded-full animate-spin"></div>
+        </div>
+    )
+}
